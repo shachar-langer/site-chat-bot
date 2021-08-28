a4f867dbb5949bc5311b8c292215ed4910b98f8d
Shachar Langer <shachar.langer@gmail.com>
2021-08-28 15:45:01 +0400

    remove About page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -17,7 +17,7 @@ import ButtonRepo from '@/components/ButtonRepo.vue'
       <div class="flex mt-8 lg:flex-shrink-0 lg:mt-0">
         <div class="inline-flex rounded-md shadow">
           <router-link
-            to="/about"
+            to="/not-found"
             class="inline-flex items-center justify-center px-5 py-3 text-base font-medium leading-6 text-white transition duration-150 ease-in-out bg-indigo-600 border border-transparent rounded-md hover:bg-indigo-500 focus:outline-none"
             >Next Page</router-link
           >
